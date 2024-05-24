9ec5890f8c5874e9ee13044aa91a5f7aeabe2c0c
Jamille <jamille.araujo.freitas2003@gmail.com>
2024-05-24 12:05:02 -0300

    third commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,39 +1,84 @@
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 
 // assets
 import image from '../assets/help-desk.png'
 
 const Login = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [formValues, setFormValues] = useState({ name: '', password: '' });
+  const [errors, setErrors] = useState('');
+  const [showPassword, setShowPassword] = useState(false)
 
+  // Function to show/hide password
   const togglePassword = () => {
     setShowPassword(!showPassword);
   };
 
-  return (    
-    <article class="flex justify-center align-center bg-slate-300 h-screen">
-      <form class="flex flex-col self-center gap-5 p-[5%] md:bg-white md:rounded-2xl md:p-[2%] md:h-[500px] md:w-[400px] ">
-        <img class="md:h-[200px]" src={image} alt="person help-desk"/>
+  // Function to handle form input changes
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
-        <div class="flex flex-col">
-          <label htmlFor="name">Nome:</label>
+  // Function to validate form
+  const validate = () => {
+    if (!formValues.name && !formValues.password) {
+      setErrors('Por favor, insira um nome e uma senha');
+      return false;
+    } else if (!formValues.name) {
+      setErrors('Por favor, insira um nome');
+      return false;
+    } else if (!formValues.password) {
+      setErrors('Por favor, insira uma senha');
+      return false;
+    }
+
+    setErrors('');
+    return true;
+  };
+
+  // Function to handle form submission
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      alert('Login realizado com sucesso!');
+    }
+  };
+
+  return (
+    <article className="flex justify-center align-center bg-slate-300 h-screen">
+      <form 
+        className="flex flex-col self-center gap-2 p-[5%] md:bg-white md:rounded-2xl md:p-[2%] md:h-auto md:w-[400px]"
+        onSubmit={handleLogin}
+      >
+        <img className="md:h-[200px]" src={image} alt="person help-desk" />
+
+        <div className="flex flex-col">
+          <label class="mb-1" htmlFor="name">Nome:</label>
           <input 
-            class="h-[40px] pl-2 focus:outline-none border-[0.2px] border-gray-300 rounded-md" 
-            type="text" 
-            id="name" 
-            name="name" 
-            placeholder="Digite seu nome" />
+            className={`h-[40px] pl-2 focus:outline-none border-[0.2px] border-gray-300 rounded-md ${!formValues.name && errors ? 'border-red-500' : ''}`}
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Digite seu nome"
+            value={formValues.name}
+            onChange={handleChange}
+          />
         </div>
 
-        <div class="flex flex-col">
-          <label htmlFor="password">Senha:</label>
+        <div className="flex flex-col">
+          <label class="mb-1" htmlFor="password">Senha:</label>
           <input 
-            class="h-[40px] pl-2 focus:outline-none border-[0.2px] border-gray-300 rounded-md"  
-            type={showPassword ? 'text' : 'password'} 
-            id="password" 
-            name="password" 
-            placeholder="Digite sua senha" />
+            className={`h-[40px] pl-2 focus:outline-none border-[0.2px] border-gray-300 rounded-md ${!formValues.password && errors ? 'border-red-500' : ''}`}
+            type={showPassword ? 'text' : 'password'}
+            id="password"
+            name="password"
+            placeholder="Digite sua senha"
+            value={formValues.password}
+            onChange={handleChange}
+          />
         </div>
+
+        <span className="mb-2 text-red-500">{errors}</span>
 
         <div>
           <input
@@ -46,8 +91,8 @@ const Login = () => {
         </div>
 
         <input 
-          class="h-[40px] w-full bg-blue-500 hover:bg-blue-600 text-white rounded-md cursor-pointer" 
-          type="submit" 
+          className="mt-1 h-[40px] w-full bg-blue-500 hover:bg-blue-600 text-white rounded-md cursor-pointer"
+          type="submit"
           value="Enviar"
         />
       </form>
